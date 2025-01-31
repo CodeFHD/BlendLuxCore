14e65670454cef4752c72e6243cfc2fbdf1f6a2c
CodeFHD <joho.hini@gmail.com>
2025-01-31 14:17:58 +0100

    Implement setting in material output node to exclude from override (BlendLuxCore/issues/547)

--- export/caches/object_cache.py
+++ export/caches/object_cache.py
@@ -129,11 +129,20 @@ def warn_about_subdivision_levels(obj):
 
 def get_material(obj, material_index, depsgraph):
     material_override = depsgraph.view_layer_eval.material_override
+    material = obj.material_slots[material_index].material
+    if material is not None:
+        node_tree = material.luxcore.node_tree
+        output_node = get_active_output(node_tree)
+        override_exclude = output_node.override_exclude
+    else:
+        override_exclude = False
 
-    if material_override:
+    if material_override and material is None:
+        mat = material_override
+    elif material_override and not override_exclude:
         mat = material_override
     elif material_index < len(obj.material_slots):
-        mat = obj.material_slots[material_index].material
+        mat = material
 
         if mat is None:
             # Note: material.convert returns the fallback material in this case
